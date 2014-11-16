0c7f97095c07bbb34fb903fba2371716293e405e
XITRIX <xitrix@bk.ru>
2014-11-16 19:05:20 +0400

    Create converter.c

---
+++ converter.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *file;
+char ch[100][1000];
+int cr[100][100];
+int cg[100][100];
+int cb[100][100];
+int gs[100][100];
+int bw[100][100];
+int g,h,i,j; // длинна, высота, дл¤ счЄта
+
+void recognize(); //считывание строк
+void colorprisv(); //присвоение цветов в массивы
+void gradient(); //создание массива градиента серого
+void binarizator(); //бинаризаци¤ серого массива
+void show(); //вывод бинаризированного массива дл¤ записи в файл
+
+int main(int argc, char *argv[])
+{
+	file = fopen(argv[1], "r");
+	if (file==NULL) {printf("Введите в аргументах путь к изображению"); exit(1);}
+	recognize();
+	colorprisv();
+	gradient();
+	binarizator();
+	show();
+}
+
+void recognize()
+{
+	h=1;
+	while (fgets(ch[h], 1000, file))
+	{
+		h++;
+	}
+}
+
+void colorprisv()
+{
+	char *buf;
+	int d=1;
+	g=1;
+	i=1;
+
+	for(j=1;j<h;j++)
+	{
+		buf = strtok(ch[j], " ");
+		while (buf != NULL)
+		{
+			if (d==1) {cr[j][i] = atoi(buf); d++;}
+			else if (d==2) {cg[j][i] = atoi(buf); d++;}
+			else if (d==3) {cb[j][i] = atoi(buf); d=1; i++;}
+			if (g<i) g=i;
+			buf=strtok(NULL," ");
+		}
+		d=1;
+		i=1;
+	}
+}
+
+void gradient()
+{
+	for(j=4;j<h;j++)
+		for(i=1;i<g;i++)
+			gs[j][i]=(cr[j][i]+cg[j][i]+cb[j][i])/3;
+}
+
+void binarizator()
+{
+	for(j=4;j<h;j++)
+		for(i=1;i<g;i++)
+		{
+			if (gs[j][i]>atoi(ch[3])/3) bw[j-3][i]=0;
+			else bw[j-3][i]=1;
+		}
+}
+
+void show()
+{
+	printf("P1\n");
+	printf("%d %d\n", h-4, g-1);
+	for(j=1;j<h-3;j++){
+		for(i=1;i<g;i++)
+		{
+			printf("%d ",bw[j][i]);
+		}
+	printf("\n");
+	}
+}
