108b866d94e818297893efcc9bafff86a7375a22
XITRIX <xitrix@bk.ru>
2015-06-01 22:59:22 +0300

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -6,36 +6,27 @@ FILE *file;
 char ch[1000][10000];
 int cr[1000][1000],h,i,j,z,x;
 
-void recognize(); //считывание строк
-void colorprisv(); //присвоение цветов в массивы
+void recognize();
+void show();
 
 int main(int argc, char *argv[])
 {
 	file = fopen(argv[1], "r");
 	if (file==NULL) {printf("ARGS!!!!"); exit(1);}
 	recognize();
-	colorprisv();
-	for(z=1;z<=cr[0][2];z++){
-		for(x=1;x<=cr[0][1];x++)
-			{printf("%d ",cr[z][x]);}
-		printf("\n");	
-	}
+	show();
 }
 
 void recognize()
 {
+	char *buf;
+	int d=1;
+	i=1;
 	h=1;
 	while (fgets(ch[h], 1000, file))
 	{
 		h++;
 	}
-}
-
-void colorprisv()
-{
-	char *buf;
-	int d=1;
-	i=1;
 
 	for(j=1;j<h;j++)
 	{
@@ -46,5 +37,13 @@ void colorprisv()
 			buf=strtok(NULL," ");
 		}
 		i=1;
+	}
+}
+
+void show(){
+	for(z=1;z<=cr[0][2];z++){
+		for(x=1;x<=cr[0][1];x++)
+			{printf("%d ",cr[z][x]);}
+		printf("\n");	
 	}
 }
